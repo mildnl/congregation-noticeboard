b1835becb569bbe62277ff482ae937d58669faa1
Markus Mildner <mail@l-mildner.com>
2023-06-06 18:45:30 +0200

    convention page

---
+++ src/components/ConventionCard.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { _ } from '../services/i18n/i18n';
+  export let conventionData = {
+    date: null,
+    location: null,
+    host: null
+  }
+</script>
+<div class="w3-container s3-margin">
+  <div class="w3-card w3-padding">
+  <table class="w3-center">
+    <tr>
+      <th>
+        {$_('date')}
+      </th>
+      <th>
+        {$_('location')}
+      </th>
+    </tr> 
+    <tr>
+      <td>
+        <h4>{conventionData.date}</h4>
+      </td>
+      <td>
+      <h4>{conventionData.location}</h4>
+      </td>
+    </tr>
+    <tr>
+      <td></td>
+      <td>
+          {$_(conventionData.host)}
+      </td>
+    </tr>
+  </table>      
+</div>
+</div>
--- src/views/Conventions.svelte
+++ src/views/Conventions.svelte
@@ -1,5 +1,19 @@
 <script>
   import { _ } from '../services/i18n/i18n';
+	import ConventionCard from '../components/ConventionCard.svelte';
+  let conventionInvites = [
+    {
+      date: '07.10.2023',
+      location: 'Stadthalle, Gütersloh',
+      host: 'conventions.host-co'
+    },
+    {
+      date: '19.05.2024',
+      location: 'Stadthalle, Gütersloh',
+      host: 'conventions.host-bethel'
+    }
+  ]
+
 </script>
 <style>
      footer {
@@ -20,8 +34,21 @@ footer a {
   margin-left: 10px;
 }
 </style>
+<div class="w3-container">
+  <h1 class="w3-center w3-padding w3-panel w3-black">{$_('menu.conventions')}</h1>
+</div>
+<div class="w3-container">
+  <h4 class="w3-center">{$_('conventions.announcment')}</h4>
 
-<h1 class="w3-center w3-padding w3-panel w3-black">{$_('menu.conventions')}</h1>
+  <div class="w3-row">
+      {#each conventionInvites as invite}
+      <div class="w3-col s6">
+        <ConventionCard conventionData={invite}/>
+      </div>
+      {/each}
+  </div>
+</div>
+
 
 <footer>
   <a href="#top" title="Home">Home</a>
