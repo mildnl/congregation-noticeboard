e83e5839c27b0a9d910548fa8575f1cdd33f06d0
Markus Mildner <mail@l-mildner.com>
2023-07-09 23:14:35 +0200

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,21 +1,8 @@
 <script lang="ts">
-  import { signOut } from "@auth/sveltekit/client"
-  import { page } from "$app/stores"
-
   import awsmobile from '../aws-exports';
 	import { Amplify } from "aws-amplify";
   Amplify.configure(awsmobile);
 </script>
 <div>
-  <header>
-    {#if $page.data.session}
-      <div>
-        <strong>Hello {$page.data.session.user?.name}</strong>
-        <button on:click|preventDefault={signOut} class="button">Sign out</button>
-      </div>
-    {:else}
-      <a href="/auth/login" class="buttonPrimary">Sign in</a>
-    {/if}
-  </header>
   <slot />
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,56 @@
   import Menu from '$lib/components/Menu.svelte';
   import { waitLocale } from 'svelte-i18n';
   import setupI18n from '$lib/services/i18n/i18n';
-  import { signIn } from "@auth/sveltekit/client"
   import { invalidateAll } from '$app/navigation';
+
+  import { Auth } from 'aws-amplify';
+
+  type SignUpParameters = {
+    username: string;
+    password: string;
+    given_name: string;
+    family_name: string;
+    email: string;
+    phoneNumber: string;
+  };
+
+  async function signUp({ username, password, email, phoneNumber, given_name, family_name }: SignUpParameters) {
+    try {
+      const { user } = await Auth.signUp({
+        username,
+        password,
+        attributes: {
+          email,
+          phoneNumber, // optional - E.164 number convention
+          given_name,
+          family_name,
+        },
+        autoSignIn: {
+          // optional - enables auto sign in after user is confirmed
+          enabled: true,
+        },
+      });
+      console.log(user);
+    } catch (error) {
+      console.log('error signing up:', error);
+    }
+  }
 
   setupI18n('de');
   export async function preload() {
     return waitLocale('de');
-  } 
-   
-  const handleSubmit = async (event: any) => {
+  }
+
+  const handleSignUp = async (event: any) => {
     const data = new FormData(event.target);
     try {
-      await signIn('credentials', {
+      await signUp({
+        username: data.get('username'),
         email: data.get('email'),
-        password: data.get('password')
+        password: data.get('password'),
+        given_name: data.get('givenName'),
+        family_name: data.get('familyName'),
+        phoneNumber: data.get('phoneNumber')
       });
     } catch (error) {
       await invalidateAll();
@@ -26,50 +62,54 @@
 
 <style>
   :global(body) {
-    background-color:whitesmoke;
+    background-color: whitesmoke;
   }
 
+  footer {
+    position: relative;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    background-color: white;
+    height: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+  }
 
-    footer {
-  position:relative;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  background-color: white;
-  height: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-}
-
-footer a {
-  color: #000;
-  margin-left: 5px;
-}
+  footer a {
+    color: #000;
+    margin-left: 5px;
+  }
 </style>
+
 <main>
-  <Menu/>
+  <Menu />
   <div id="main" class="w3-container">
     <div class="w3-container w3-white w3-padding-16">
       <div class="w3-row">
 
-       <div class="w3-center">
-         <h1 in:scale out:fade>Wilkommen</h1>
-        <h1>Login</h1>
+        <div class="w3-center">
+          <h1 in:scale out:fade>Wilkommen</h1>
+          <h1>Login</h1>
 
-        <div>
-          <form name="login" method="POST" on:submit|preventDefault={handleSubmit}>
-            <input name="email" type="email" placeholder="Email Address" />
-            <input name="password" placeholder="Password" type="password" />
-            <button>Sign In</button>
-          </form>
+          <div>
+            <form name="login" method="POST" on:submit|preventDefault={handleSignUp}>
+              <input name="username" placeholder="Username" />
+              <input name="email" type="email" placeholder="Email Address" />
+              <input name="password" placeholder="Password" type="password" />
+              <input name="givenName" placeholder="Given Name" />
+              <input name="familyName" placeholder="Family Name" />
+              <input name="phoneNumber" type="tel" placeholder="Phone Number" />
+              <button>Sign Up</button>
+            </form>
+          </div>
         </div>
-       </div>
 
+      </div>
     </div>
   </div>
 </main>
-
 
 <footer class="w3-container w3-center w3-padding">
   <a href="https://www.flaticon.com/de/kostenlose-icons/diamant" title="diamant Icons">Diamant</a>
